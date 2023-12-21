121616bbed965e24b14467fbf6bc521d613d04f2
Helmi Ahmad Khaderani <helmikhaderani@gmail.com>
2023-12-21 11:03:49 +0700

    api android & ml

---
+++ database.py
@@ -0,0 +1,256 @@
+import sqlalchemy, os
+from dotenv import load_dotenv
+from sqlalchemy import inspect, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
+import bcrypt
+from datetime import datetime
+
+load_dotenv()
+
+
+def get_database_engine():
+    while True:
+        username = os.getenv('USER')
+        password = os.getenv('PASSWORD')
+        host = os.getenv('HOST')
+        port = os.getenv('PORT')
+        dbname = os.getenv('DBNAME')
+        engine = sqlalchemy.create_engine(
+            "mysql+pymysql://" + username + ":" + password + "@" + host + ":" + port + "/" + dbname)
+
+        try:
+            print("Connection to the database successful!")
+            print(engine)
+            return engine
+        except Exception as e:
+            print("Error connecting to the database:", e)
+            print("Please try again.\n")
+
+
+engine = get_database_engine()
+Base = declarative_base()
+
+
+def get_table_names(engine=engine):
+    table_names = inspect(engine).get_table_names()
+    return table_names
+
+
+print(get_table_names())
+
+Session = sqlalchemy.orm.sessionmaker()
+Session.configure(bind=engine)
+session = Session()
+
+
+class User(Base):
+    __tablename__ = 'user'
+    username = Column(String(100), primary_key=True)
+    password = Column(String(100))
+    name = Column(String(100))
+    email = Column(String(100))
+    point = Column(Integer, default=0)
+
+    histories = relationship("History", back_populates="user")
+
+    def set_password(self, password):
+        self.password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.checkpw(password.encode('utf-8'), self.password.encode('utf-8'))
+
+
+class History(Base):
+    __tablename__ = 'history'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(100), ForeignKey('user.username'))
+    foto = Column(String(1000))
+    timestamp = Column(String(100))
+    jenis_sampah = Column(String(100))
+
+    user = relationship("User", back_populates="histories")
+
+
+Base.metadata.create_all(engine)
+
+
+# users = session.query(User).all()
+# for user in users:
+#     print(user.point)
+#
+# histories = session.query(History).all()
+# for history in histories:
+#     print(history.username)
+
+# select table query
+def select_all(table):
+    results = session.query(table).all()
+    return results
+
+
+def user_exists(usernameORemail, session=session):
+    return session.query(User).filter(
+        (User.username == usernameORemail) | (User.email == usernameORemail)
+    ).first() is not None
+
+
+def add_user(data):
+    new_user = User(
+        username=data['username'],
+        name=data['name'],
+        email=data['email']
+    )
+    # set password and hash that
+    new_user.set_password(data['password'])
+
+    session.add(new_user)
+    session.commit()
+
+
+def login_user(data):
+    user = session.query(User).filter_by(username=data['username']).first()
+    if user and user.check_password(data['password']):
+        return True
+    else:
+        return False
+
+
+def add_history(username, foto, jenis_sampah):
+    user = session.query(User).filter_by(username=username).first()
+    if user:
+        # Add points jika jenis_sampah
+        if jenis_sampah.lower() in ['cardboard', 'paper']:
+            user.point += 10
+        if jenis_sampah.lower() in ['plastic']:
+            user.point += 15
+        if jenis_sampah.lower() in ['glass', 'metal']:
+            user.point += 20
+
+    new_data = History(
+        username=username,
+        foto=foto,
+        timestamp=datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        jenis_sampah=jenis_sampah
+    )
+
+    session.add(new_data)
+    session.commit()
+
+def get_history_user(username):
+    history_records = session.query(History).filter_by(username=username).all()
+
+    history_list = [
+        {
+            "username": record.username,
+            "foto": record.foto,
+            "timestamp": record.timestamp,
+            "jenis_sampah": record.jenis_sampah
+        } for record in history_records
+    ]
+
+    return history_list
+
+def get_history_user_and_jenis_sampah(username, jenis_sampah):
+    history_records = session.query(History).filter(
+        History.username == username,
+        History.jenis_sampah == jenis_sampah
+    ).all()
+
+    history_list = [
+        {
+            "username": record.username,
+            "foto": record.foto,
+            "timestamp": record.timestamp,
+            "jenis_sampah": record.jenis_sampah
+        } for record in history_records
+    ]
+
+    return history_list
+
+# def filter_result(table_str, matrixColumn, matrixData):
+#     # bisa mengambil data pada table dengan hanya sebuah string
+#
+#     # table_class = globals().get(table_str.title())
+#     # if not table_class:
+#     #     raise ValueError(f"Table class '{table_str}' not found.")
+#
+#     result = session.query(cls[table_str])
+#     for i in range(len(matrixColumn)):
+#         if matrixColumn[i] == "jenis":  # SOLUSI SEMENTARA UNTUK KOLOM DENGAN NAMA NYA LEBIH DARI SATU KATA
+#             matrixColumn[i] = "jenis_perangkat"
+#         # new_result = result.filter(getattr(cls[table_str], matrixColumn[i]) == matrixData[i])
+#         new_result = result.filter(getattr(cls[table_str], matrixColumn[i]).like('%' + matrixData[i] + '%'))
+#         result = new_result
+#     return result
+#
+#
+# def getTableColumns(table_name):
+#     table = inspect(engine).get_columns(table_name)
+#     column_names = [column['name'] for column in table]
+#     return column_names
+#
+#
+# def get_table_privilege():
+#     tables = session.query(cls['table_lists']).all()
+#     result = {}
+#     for table in tables:
+#         result[table.table_name] = table.privilege  # ini masih kurang dynamic,, misal nama kolomnya berubah
+#     return result
+#
+#
+# def add_contact(lokasi, pic, kontak):
+#     new_contact = cls['contacts'](lokasi=lokasi, pic=pic, kontak=kontak)
+#     session.add(new_contact)
+#     session.commit()
+#     # return [lokasi, pic, kontak]
+#
+#
+# def addUser(nama, role, telegramid):
+#     newUser = cls['users'](nama_telegram=nama, role=role, telegram_id=telegramid)
+#     session.add(newUser)
+#     session.commit()
+#     return [nama, role]
+#
+#
+# def select_all_users():
+#     employees = session.query(cls['users']).all()
+#     for employee in employees:
+#         employeename = " - " + employee.nama_telegram + ' ' + employee.role
+#         print(employeename)
+#
+#
+# def selectByRole(telegramid):
+#     users = session.query(cls['users']).filter_by(telegram_id=telegramid)
+#     return users
+#
+#
+# def updateUserStatus(id, isActive):
+#     employee = session.get(cls['users'], id)
+#     employee.active = isActive
+#     session.commit()
+#
+#
+# def deleteUser(id):
+#     session.query(cls['users']).filter(cls['users'].id == id).delete()
+#     session.commit()
+#
+#
+# def summary_table(table, column, select_value, summary_columns):
+#     summary_dictionary = {}
+#     for summary_column in summary_columns:
+#         query = f"""
+#                 SELECT {column}, {summary_column}, COUNT(*)
+#                 FROM  {table}
+#                 WHERE {column} = :{column}
+#                 GROUP BY {summary_column}
+#                 """
+#         with engine.connect() as connection:
+#             results = connection.execute(text(query), {column: select_value}).fetchall()
+#
+#         column_name = {}
+#         for value in results:
+#             column_name[value[1]] = value[2]
+#
+#         summary_dictionary[summary_column] = column_name
+#
+#     return summary_dictionary
--- main.py
+++ main.py
@@ -1,29 +1,103 @@
-from io import BytesIO
-import tensorflow as tf
+import io
+import os
+
 from tensorflow import keras
 import numpy as np
+from database import *
 from PIL import Image
 from flask import Flask, request, jsonify
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import jwt_required, JWTManager, get_jwt_identity
+from datetime import timedelta
+from google.cloud import storage
+from werkzeug.utils import secure_filename
+from functions import *
+import uuid
+
+load_dotenv()
 
 app = Flask(__name__)
 
+# config JWT
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_KEY')
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=1)
+
+jwt = JWTManager(app)
+
+#inlitialize google cloud storage bucket
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "key/garbage-classification-408613-0f1ae06ae751.json"
+
+client = storage.Client()
+bucket_name = os.getenv('BUCKET_NAME')
+
+#inlitialize model
 model = keras.models.load_model("model.h5")
 class_names = ['cardboard', 'glass', 'metal', 'paper', 'plastic']
+
+@jwt.expired_token_loader
+def expired_token_callback(jwt_header, jwt_payload):
+    return jsonify({"error": "Token has expired"}), 401
 
 @app.route('/', methods=['GET'])
 def home():
     return "API is running!"
 
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.json
+    if not data or not all(key in data for key in ['username', 'password', 'name', 'email']):
+        return jsonify({'message': 'Missing data'}), 400
+
+    if user_exists(data['username']) | user_exists(data['email']):
+        return jsonify({'message': 'Username or Email already exists'}), 409
+
+    add_user(data)
+
+    return jsonify({'message': 'User registered successfully'}), 201
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    if not data or not all(key in data for key in ['username', 'password']):
+        return jsonify({'message': 'Missing data'}), 400
+
+    if login_user(data):
+        access_token = create_access_token(identity=data['username'])
+        return jsonify(access_token=access_token), 200
+    else:
+        return jsonify({"message": "Invalid username or password"}), 401
+
 @app.route('/sendpicture', methods=['POST'])
+@jwt_required()
 def sendpicture():
+    current_user = get_jwt_identity()
     file = request.files.get('image')
+
     if file is None or file.filename == "":
         return jsonify({"error": "no file"})
 
-    try:
-        image = file.read()
-        img = Image.open(BytesIO(image))
+    if not allowed_file(file.filename):
+        return jsonify({"error": "Invalid file type"})
 
+    try:
+        filename = secure_filename(file.filename)
+        object_name = f"{current_user}/{filename}"
+
+        in_memory_file = io.BytesIO()
+        file.save(in_memory_file)
+        in_memory_file.seek(0)
+
+        # Access the bucket
+        bucket = client.bucket(bucket_name)
+
+        # Get the blob
+        blob = bucket.blob(object_name)
+
+        blob.upload_from_file(in_memory_file, content_type=file.content_type)
+
+        # Process the image as needed
+        in_memory_file.seek(0)  # Reset file pointer
+        img = Image.open(in_memory_file)
         input_shape = (256, 256)
         img = img.resize(input_shape)
         img_array = np.array(img) / 255.0
@@ -38,13 +112,49 @@ def sendpicture():
         print("Probability: ", output[0][prediction])
 
         data = {
-                "prediction": class_names[prediction],
-                "probability": float(output[0][prediction])
-                }
+            "prediction": class_names[prediction],
+            "accuracy": float(output[0][prediction]),
+            "image_url": blob.public_url
+        }
+
+        add_history(current_user, blob.public_url, class_names[prediction])
 
         return jsonify(data)
     except Exception as e:
         return jsonify({"error": str(e)})
 
+@app.route('/result', methods=['GET'])
+@jwt_required()
+def gethistory():
+    try:
+        current_user = get_jwt_identity()
+        history_list = get_history_user(current_user)
+        return jsonify(history_list)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/result/<jenis_sampah>', methods=['GET'])
+@jwt_required()
+def get_filtered_history(jenis_sampah):
+    try:
+        current_user = get_jwt_identity()
+        history_list = get_history_user_and_jenis_sampah(current_user, jenis_sampah)
+        return jsonify(history_list)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+#error handling
+@app.errorhandler(404)
+def not_found_error(error):
+    return jsonify({"error": "Resource not found"}), 404
+
+@app.errorhandler(400)
+def bad_request_error(error):
+    return jsonify({"error": "Bad request"}), 400
+
+@app.errorhandler(500)
+def internal_error(error):
+    return jsonify({"error": "Internal server error"}), 500
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8080)
